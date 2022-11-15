9e7eaae1cd9f06ca3b154967d58b39096cc5e415
username-catcat <100233678+username-catcat@users.noreply.github.com>
2022-11-15 23:17:26 +0700

    Update PythonApplication1.py

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -5,11 +5,12 @@ power=50
 height=0.0
 dtime=0.001
 temp=0.0
+first= True
 i=0
 print (" insert acelleration lasting time")
 ALT=float(input())
 while(True):
-    height1=height
+    maxheight=height
     acelleration=(power/mass)-g
     velocity=velocity+(acelleration*dtime)
     height= height+(velocity*dtime)
@@ -18,9 +19,10 @@ while(True):
     if (temp>=ALT):
         power=0
         
-    if(height1>height):
-        print ("max heigth" + str(height1))
+    if(maxheight>height and first):
+        print ("max heigth" + str(maxheight))
+        first=False
     if (height<=0 and i>1):
-        print (velocity)
+        print (str(velocity) + "    " + str(maxheight))
         break
     i+=1
